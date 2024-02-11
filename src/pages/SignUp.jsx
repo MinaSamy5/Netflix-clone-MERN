c75227cc658c332c5d606f8b8a3a8d3687b61f33
Mina Samy Samir Milad <M.S.MILAD@student.aast.edu>
2024-02-11 03:07:46 +0200

    Updates added to the SignUp component

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,34 +1,82 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
 import BackgroundImage from "../components/BackgroundImage";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  return( 
-  <Container>
-    <BackgroundImage />
-    <div className="content">
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate()
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    
-    <Header login />
-    <div className="body flex column a-center j-center">
-    <div className="text flex column">
-    <h1>Unlimited movies, TV shows, and more.</h1>
-    <h4>Watch anywhere. Cancel anytime.</h4>
-    <h6>Ready to watch? Enter your email to create an account or restart membership</h6>
-    </div>
-    <div className="form">
-        <input type="email" placeholder="Email Address" name = "email" />
-        <input type="password" placeholder="Password" name = "password" />
-        <button>Get Started</button>
-    </div>
-    <button>Log In</button>
-    </div>
-    </div>
-  </Container>
-  ); 
+  const handleSignIn = async () => {
+    try{
+      const {email, password} = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth,email,password);
+    }
+    catch(err){
+      console.log(err)
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (currentUser)=> {
+    if(currentUser) navigate("/");
+  });
+
+  return (
+    <Container showPassword={showPassword}>
+      <BackgroundImage />
+      <div className="content">
+
+
+        <Header login />
+        <div className="body flex column a-center j-center">
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <h4>Watch anywhere. Cancel anytime.</h4>
+            <h6>Ready to watch? Enter your email to create an account or restart membership</h6>
+          </div>
+          <div className="form">
+            <input type="email" placeholder="Email Address"  onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              name="email"
+              value={formValues.email} />
+           {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                name="password"
+                value={formValues.password}
+              />
+            )}
+
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
+
+          </div>
+          {showPassword && <button onClick={handleSignIn}>Log In</button>}
+        </div>
+      </div>
+    </Container>
+  );
 }
-
 const Container = styled.div`
 position: relative;
 .content{
@@ -40,5 +88,50 @@ position: relative;
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1{
+          padding: 0 25 rem;
+        }
+      }
+      .form{
+        display: grid;
+        grid-template-columns:${({ showPassword }) => showPassword ? "1fr 1fr" : "2fr 1fr"}; ;
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font_size: 1.2rem;
+          border: 1px solid black;
+          &: focus{
+            outline: none;
+          }
+        }
+        button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        font-weight: bolder;
+        font-size: 1.05rem;
+        }
+      }
+      button{
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius: 0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
 }
 `;
