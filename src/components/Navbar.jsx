2bbecd9bda492bc523f0811210f7ad88bd0e4c2d
Mina Samy Samir Milad <M.S.MILAD@student.aast.edu>
2024-02-15 03:42:24 +0200

    Added the home page component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -154,9 +154,9 @@
 
 
 
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/logo.png";
 import { firebaseAuth } from "../utils/firebase-config";
@@ -170,6 +170,12 @@ export default function Navbar({ isScrolled }) {
     { name: "Movies", link: "/movies" },
     { name: "My List", link: "/mylist" },
   ];
+
+  const navigate = useNavigate();
+
+  onAuthStateChanged(firebaseAuth, (currentUser)=> {
+    if(!currentUser) navigate("/login");
+  });
 
   return (
     <Container>
